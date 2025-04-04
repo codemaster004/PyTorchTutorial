e0224ceb5393a767fa56a2c0b0fe684540481833
Filip Dabkowski <filip.dabkowski@gmail.com>
2025-04-04 22:13:58 +0200

    current version

--- 04_cnn_handwriting/train.py
+++ 04_cnn_handwriting/train.py
@@ -1,29 +1,28 @@
 import os
 import time
 
+import pandas as pd
 import torch
 import torch.nn as nn
-from PIL import Image
 from torch.optim import SGD
 from torch.utils.data import DataLoader
+from sklearn.model_selection import train_test_split
 
 from cnn import CNN
 from dataset import PLWordsDataset, transformation
 
 N_EPOCHS = 500
-BATCH_SIZE = 32
+BATCH_SIZE = 48
 LEARNING_RATE = 1e-3
-MOMENTUM = 0.9
+MOMENTUM = 0.0
 
 DEVICE = "mps"
 
 LOG_INTERVAL = 1
-SAVE_STEP = 100
+SAVE_STEP = 20
 
 
-def train(loader, device):
-	model = CNN(len(loader)).to(device)
-	
+def train(model, loader, device):
 	criterion = nn.CrossEntropyLoss()
 	optimizer = SGD(model.parameters(), lr=LEARNING_RATE, momentum=MOMENTUM)
 	
@@ -36,7 +35,7 @@ def train(loader, device):
 		correct = 0
 		
 		for images, labels in loader:
-			images, labels = images.to(DEVICE), labels.to(DEVICE)
+			images, labels = images.to(device), labels.to(device)
 			
 			outputs = model(images)
 			loss = criterion(outputs, labels)
@@ -60,10 +59,21 @@ def train(loader, device):
 			print(f"Speed: {str(round(duration / LOG_INTERVAL, 1)) + ' s/it' if duration / LOG_INTERVAL > 1.0 else str(round(LOG_INTERVAL / duration)) + ' it/s'}")
 		if epoch % SAVE_STEP == 0:
 			torch.save(model.state_dict(), f'steps/{str(epoch)}.pth')
-		
+
+
+def split():
+	df = pd.read_csv("./data/dataset.csv")
+	train_df, val_df = train_test_split(df, test_size=0.4, random_state=42, shuffle=True)
+	train_df.to_csv("./data/train.csv", index=False)
+	val_df.to_csv("./data/val.csv", index=False)
+	
 
 if __name__ == '__main__':
 	os.makedirs("steps", exist_ok=True)
-	dataset = PLWordsDataset("./data/dataset.csv", transform=transformation)
+	split()
+	dataset = PLWordsDataset("./data/train.csv", transform=transformation)
 	data_loader = DataLoader(dataset, batch_size=BATCH_SIZE, shuffle=True)
-	train(data_loader, DEVICE)
+	
+	model = CNN(len(dataset)).to(DEVICE)
+	model.load_state_dict(torch.load('steps/40.pth'))
+	train(model, data_loader, DEVICE)
